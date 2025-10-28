02debf56b8da35f5d402d3b5debd371da5414a0e
gb2014 <geethanjali.basker@gmail.com>
2025-10-28 15:20:33 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Hello {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, World!");
+    }
+
+}
