99690325d047b378d283e9dcaf2e493f0e9b9dee
gb2014 <geethanjali.basker@gmail.com>
2025-10-28 15:21:06 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         Date currentDate = new Date();
         System.out.println("Hello, World!");
+        System.out.println("Date : " + currentDate); 
     }
+
 
 }
